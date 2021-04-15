15976a3d3d055906c2e11ed3faf00be2442b8644
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2021-04-16 06:03:07 +0900

    java calendar add

--- src/main/java/re/sta/rt/calendar/CalendarEx1.java
+++ src/main/java/re/sta/rt/calendar/CalendarEx1.java
@@ -1,6 +1,26 @@
 package re.sta.rt.calendar;
 
 
+// 날짜와 시간 & 형식화
+
+
+/*
+    Calendar 와 Date
+    DecimalFormat
+    SimpleDateFormat
+    ChoiceFormat
+    MessageFormat
+    java.time package
+    LocalDate, LocalTime
+    Instant
+    LocalDateTime, ZonedDateTime
+    TemporalAdjusters
+    Period, Duration
+    parsing, fomat
+ */
+
+
+
 import java.util.Calendar;
 
 public class CalendarEx1 {
@@ -14,10 +34,11 @@ public class CalendarEx1 {
         System.out.println("이 해의 몇 째주" + today.get(Calendar.WEEK_OF_YEAR));
 
 
-        System.out.println(today.get(Calendar.DATE));
-        System.out.println(today.get(Calendar.DAY_OF_MONTH));
-        System.out.println(today.get(Calendar.DAY_OF_WEEK)); // 일요일부터 시작!! 
-        System.out.println(today.get(Calendar.WEEK_OF_YEAR));
+        System.out.println("이 달의 몇 일 : " + today.get(Calendar.DATE));
+        System.out.println("이 달의 몇 일 : " + today.get( Calendar.DAY_OF_MONTH));
+        System.out.println("요일(1~7), 1: 일요일 : " + today.get(Calendar.DAY_OF_WEEK)); // 일요일부터 시작!!
+
+
 
     }
 
