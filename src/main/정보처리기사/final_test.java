eb2aa24563edb49a873563088db8c30dbd303f02
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2021-04-24 15:14:56 +0900

    final(2)6tan

--- src/main/정보처리기사/final_test.java
+++ src/main/정보처리기사/final_test.java
@@ -1,11 +1,33 @@
 public class final_test {
     public static void main(String[] args) {
-        char a = 'B';
+        int i, j;
 
-        System.out.println(
+        int temp;
 
-                (char)(a-1)
+        int [] a = {5, 2, 6, 3, 7};
+        for(i=1; i<5; i++) {
+            j = i - 1;
 
-        );
+            /*
+                i = 1/ j = 0
+                i = 2/ j = 1
+                i = 3/ j = 2
+                i = 4/ j = 3
+             */
+
+            while ((j >= 0)&& (a[j] > a[j+1])) {
+                // a[0] 과 a[1]를 비교해서 왼쪽에있는 수가 크면 while문 입장
+
+
+                temp = a[j];
+                a[j] = a[j+1];
+                a[j+1] = temp;
+
+                j--;
+            }
+        }
+        for(i = 0; i < 3; i++) {
+            System.out.println(a[i] + " ");
+        }
     }
 }
